fdf3edb589c9f83c9be077409183eb53907ecb00
Oleksandr Riabtsun <oleksandr.riabtsun@paysera.net>
2024-03-11 17:21:03 +0100

    Fixed node name

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,8 +17,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder('evp_banking_history_client');
-        $rootNode = method_exists($treeBuilder, 'getRootNode') ? $treeBuilder->getRootNode() : $treeBuilder->root('evp_banking_history_client');
+        $treeBuilder = new TreeBuilder('evp_gsms');
+        $rootNode = method_exists($treeBuilder, 'getRootNode') ? $treeBuilder->getRootNode() : $treeBuilder->root('evp_gsms');
 
         $rootNode
             ->children()
